6907e4d2fbd8335b8e31ccf3c180dd06d9d92fbd
26TassioFernandes <tassiofernandes881@gmail.com>
2024-01-23 10:20:37 -0300

    Update script.js

--- src/JS/script.js
+++ src/JS/script.js
@@ -18,20 +18,30 @@ typeWrite(titulo);
 
 // Mexendo com o Navbar
 
-document.addEventListener("DOMContentLoaded", function () {
+  document.addEventListener("DOMContentLoaded", function () {
 
-  // Obtém todos os links da barra de navegação
-  var navLinks = document.querySelectorAll(".nav-item.nav-link");
+    // Obtém o botão de navegação (hamburguer)
 
-  // Adiciona um ouvinte de eventos a cada link
-  navLinks.forEach(function (link) {
-    link.addEventListener("click", function () {
+    var navbarToggler = document.getElementById("botao-navbar");
 
-      // Fecha o menu de navegação ao clicar em um link
-      var navbarToggler = document.getElementById("botao-navbar");
-      if (navbarToggler) {
-        navbarToggler.click();
-      }
-    });
+    // Verifica se o botão de navegação existe e está visível
+
+    if (navbarToggler && window.getComputedStyle(navbarToggler).display === "block") {
+
+      // Obtém todos os links da barra de navegação
+      
+      var navLinks = document.querySelectorAll(".nav-item.nav-link");
+
+      // Adiciona um ouvinte de eventos a cada link
+
+      navLinks.forEach(function (link) {
+        link.addEventListener("click", function () {
+
+          // Fecha o menu de navegação ao clicar em um link
+
+          navbarToggler.click();
+        });
+      });
+    }
   });
-});
+
