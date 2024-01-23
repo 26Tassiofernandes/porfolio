72c5410ecafff0e0746fef5bbc6932786f90c3a7
26TassioFernandes <tassiofernandes881@gmail.com>
2024-01-23 09:37:50 -0300

    update html

--- src/JS/script.js
+++ src/JS/script.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // Adiciona um ouvinte de eventos a cada link
   navLinks.forEach(function (link) {
     link.addEventListener("click", function () {
-      
+
       // Fecha o menu de navegação ao clicar em um link
       var navbarToggler = document.getElementById("botao-navbar");
       if (navbarToggler) {
